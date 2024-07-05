4c0394b1b50ed7c8fcdd208c5e9b107bfb71ceb2
DaniLoAg88 <danilopezag@gmail.com>
2024-07-05 11:20:20 +0200

    Terminado3

--- js/nacional.js
+++ js/nacional.js
@@ -1,18 +1,15 @@
 let flechas = document.querySelector("#flechasTexto");
-let bandera = true; //Siempre arranca en true porque al cargar la página los párrafos tienen contenido
+let bandera = false; //Siempre arranca en false porque al cargar la página la imagen no aparece
 
 flechas.addEventListener("click", function(){
     
-    let parrafo1 = document.querySelector("#parrafo1");
-    let parrafo2 = document.querySelector("#parrafo2");
+    let imagen = document.querySelector(".fotoTarifas");
 
     if(bandera){
-        parrafo1.textContent = "";
-        parrafo2.textContent = "";
+        imagen.src = "";
         bandera = false;
     } else{
-        parrafo1.textContent = "Ahora, gracias a los envíos nacionales de uShip, puedes enviar tus paquetes a cualquier punto del país."
-        parrafo2.textContent = "Consulta nuestras tarifas.";
+        imagen.src="img/Nacional.png";
         bandera = true;
     }
 
